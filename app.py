2e06df5884626438fb2b96139ab78c0fe0eafb26
Your Name <escarment.brandon96@gmail.com>
2026-05-04 15:19:06 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello from my app 🚀"
+
+@app.route("/health")
+def health():
+    return "OK"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=3000)
